f9f1efa9dcdab45cfef03059d3b1ca994db1b5bf
mulagi <otingajacton@gmail.com>
2023-04-26 17:42:35 +0300

    seo

--- pages/index.js
+++ pages/index.js
@@ -9,19 +9,22 @@ import Skills from '../components/Skills'
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Mulagi | Software Developer</title>
-        <meta name="description" content="I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences." />
-      
-        <meta property="og:title" content="best developer in Kenya">
-        </meta>
-        <meta property="og:description" content="Best practices in software development">
-        </meta>
-        <meta property="og:url" content="">
-
-        </meta><meta property="og:type" content="website"></meta>
-        <link rel="icon" href="/mulagiLogo.png" />
-      </Head>
+       <head>
+    <meta charset="utf-8" />
+    <title>Jackton Mulagi - Fullstack Software Engineer in Nairobi, Kenya</title>
+    <meta name="description" content="I am a skilled fullstack software engineer based in Nairobi, Kenya. My expertise includes JavaScript, React, Next.js, Java, Spring Boot, MySQL, Figma, Django, and Node.js." />
+    <meta property="og:url " content="http://www.jacktonmulagi.com/" />
+    <meta property="og:title" content="Jackton Mulagi - Fullstack Software Engineer in Nairobi, Kenya" />
+    <meta property="og:site_name" content="Jackton Mulagi" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="http://www.jacktonmulagi.com/assets/fav.png" />
+    <meta property="og:locale" content="en_KE" />
+    <meta property="og:description" content="I am a skilled fullstack software engineer based in Nairobi, Kenya. My expertise includes JavaScript, React, Next.js, Java, Spring Boot, MySQL, Figma, Django, and Node.js." />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta name="description" content="As a highly skilled fullstack software engineer based in Nairobi, Kenya, I specialize in JavaScript, React, Next.js, Java, Spring Boot, MySQL, Figma, Django, and Node.js development." />
+    <link rel="icon" href="/assets/fav.png" />
+</head>
     <Main />
     <About />
     <Skills />
