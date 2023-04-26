13fb34e909d657619addffe0b8c4511429158d1c
mulagi <otingajacton@gmail.com>
2023-04-26 18:09:13 +0300

    chnages

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import Skills from '../components/Skills'
 export default function Home() {
   return (
     <div>
-       <head>
+       <Head>
     <meta charset="utf-8" />
     <title>Jackton Mulagi - Fullstack Software Engineer in Nairobi, Kenya</title>
     <meta name="description" content="I am a skilled fullstack software engineer based in Nairobi, Kenya. My expertise includes JavaScript, React, Next.js, Java, Spring Boot, MySQL, Figma, Django, and Node.js." />
@@ -17,14 +17,14 @@ export default function Home() {
     <meta property="og:title" content="Jackton Mulagi - Fullstack Software Engineer in Nairobi, Kenya" />
     <meta property="og:site_name" content="Jackton Mulagi" />
     <meta property="og:type" content="website" />
-    <meta property="og:image" content="http://www.jacktonmulagi.com/assets/fav.png" />
+    <meta property="og:image" content="http://www.jacktonmulagi.com/assets/navLogo.svg" />
     <meta property="og:locale" content="en_KE" />
     <meta property="og:description" content="I am a skilled fullstack software engineer based in Nairobi, Kenya. My expertise includes JavaScript, React, Next.js, Java, Spring Boot, MySQL, Figma, Django, and Node.js." />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
     <meta name="description" content="As a highly skilled fullstack software engineer based in Nairobi, Kenya, I specialize in JavaScript, React, Next.js, Java, Spring Boot, MySQL, Figma, Django, and Node.js development." />
-    <link rel="icon" href="/assets/fav.png" />
-</head>
+    <link rel="icon" href="assets/navLogo.svg" />
+</Head>
     <Main />
     <About />
     <Skills />
